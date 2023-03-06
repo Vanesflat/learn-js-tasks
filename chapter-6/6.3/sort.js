79cf7dcae0316c3c07299e2dde4c9ff3be83c3a0
Vanesflat <ptptcptptc@yandex.ru>
2023-03-06 16:54:43 +0700

    6.3 tasks done

---
+++ chapter-6/6.3/sort.js
@@ -0,0 +1,11 @@
+const users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+
+function sortByField(fieldName) {
+  return function (a, b) {
+    return a[fieldName] > b[fieldName] ? 1 : -1;
+  }
+}
